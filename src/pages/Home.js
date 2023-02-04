e52e658ad0a37885420c85da5ace2c0e97122d52
Yurii Rybachok <drummer2009@ukr.net>
2023-02-04 19:12:42 +0200

    add buttons to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
 import RoomInfoCard from "../components/RoomInfoCard";
 import style from '../style/home.module.css';
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid3X3Gap } from "react-icons/bs";
+import data from '../data/roomCard.json';
+import BottomSheet from "../components/BottomSheet";
 
 export default function Home() {
+    const [itemsVisible, setItemsVisible] = useState(true);
+    const [sheetVisible, setSheetVisible] = useState(false);
+    const [sheetCreateRoom, setSheetCreateRoom] = useState(false);
+    const [loaderVisibility, setLoaderVisibility] = useState(false);
+    const [cardId, setCardId] = useState(1);
     return (
         <>
             <Header />
@@ -12,6 +21,29 @@ export default function Home() {
                 <DailyInfoCard />
                 <RoomInfoCard />
             </div>
+            <div className={style.action_btn}>
+                <button onClick={() => setSheetVisible(true)}>
+                    <AiOutlinePlus className="mr-2" />
+                    Start a room
+                </button>
+                <button>
+                    <BsGrid3X3Gap />
+                </button>
+            </div>
+            <BottomSheet 
+                sheetTitle = 'start room'
+                setSheetVisible={(item) => setSheetVisible(item)}
+                sheetVisible={sheetVisible}
+                cardDetail={data.find((item) => item.id == cardId)}
+                setItemsVisible={(item) => setItemsVisible(item)}
+                setSheetCreateRoom={(item) => {
+                    setLoaderVisibility(true);
+                    setTimeout(() => {
+                        setSheetCreateRoom(item);
+                        setLoaderVisibility(false);
+                    }, 1000);
+                }}
+            />
         </>
     )
 }
