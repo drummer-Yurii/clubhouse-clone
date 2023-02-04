b0c1b89497a8565fde82ede14a7405b13fe914d1
Yurii Rybachok <drummer2009@ukr.net>
2023-02-04 09:09:24 +0200

    add phone confirmation page

---
+++ src/pages/PhoneConfirmation.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import style from "../style/phoneConfirm.module.css";
+import { Link } from "react-router-dom";
+import PhoneInput from "react-phone-number-input";
+
+export default function PhoneConfirmation() {
+    const [value, setValue] = useState();
+
+    return (
+        <div className={style.phoneConfirmContainer}>
+            <Link exact="true" to='/' className={style.backBtn}>
+                <img src="/images/arrow.png" alt="" />
+            </Link>
+            <h1>Enter you phone</h1>
+            <PhoneInput international defaultCountry="US"
+            value={value} onChange={setValue} />
+            <p>By entering your number, you're agreeing to our {" "}
+                <span>Terms of Service and Privacy Policy.</span>
+                Thanks!
+            </p>
+            <Link exact="true" to="/code_confirm" className="primaryBtn d-flex align-items-center">
+                Next <img src="/images/nextArrowIcon.svg" className="ml-1" alt="" />
+            </Link>
+        </div>
+    )
+}
