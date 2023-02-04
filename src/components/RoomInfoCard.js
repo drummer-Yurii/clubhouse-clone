1a299c3af50a1ec2d1d409236c16271e619a508b
Yurii Rybachok <drummer2009@ukr.net>
2023-02-04 15:57:49 +0200

    add room card component

---
+++ src/components/RoomInfoCard.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import style from '../style/roomCard.module.css';
+import data from '../data/roomCard.json';
+
+export default function RoomInfoCard(props) {
+    return (
+        <>
+            {/* <h2>I am the room info card</h2> */}
+            {data.map((item) => (
+                <div>
+                    <div>
+                        <div className={style.roomCardContainer}>
+                            <h6>{item.title}</h6>
+                            <h2>{item.sub_title}</h2>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
